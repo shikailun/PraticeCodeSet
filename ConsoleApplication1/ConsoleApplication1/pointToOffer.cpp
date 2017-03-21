c605c4d6f6119ce753fd99d928d5bdcc7ea6fb0b
skaren <shikailun_skl@163.com>
2017-03-21 16:39:49 +0800

    最新 2017 03.21

--- ConsoleApplication1/ConsoleApplication1/pointToOffer.cpp
+++ ConsoleApplication1/ConsoleApplication1/pointToOffer.cpp
@@ -21,30 +21,32 @@
 	return count;
 }*/
 
-
-void Solution::replaceSpace(char *str, int length) 
+//将字符串中的' '替换成'%20'
+/*void Solution::replaceSpace(char *str, int length) 
 {
-	if (str == NULL || length == 0)
+	if (str == NULL || length <= 0)
 	{
 		return;
 	}
 	int count = 0;
 	int i = 0, j = 0;
+	int oldnumber = 0;
 	while (str[i] != '\0')
 	{
+		oldnumber++;
 		if (*str == ' ')
 			count++;
 		i++;
 	}
 
-	int oldLength = i;
-	int newLength = i + count * 2;
+	int oldLength = oldnumber;
+	int newLength = oldnumber + count * 2;
 	if (newLength > length)
 	{
 		return;
 	}
 	
-
+	 
 	while (oldLength >= 0 && newLength > oldLength )
 	{
 		if (str[oldLength] != ' ')
@@ -62,8 +64,54 @@ void Solution::replaceSpace(char *str, int length)
 
 	}
 
+}*/
+//用两个栈实现队列
+/*void Solution::push(int node) {
 
-
-	
+	stack1.push(node);
 
 }
+
+int Solution::pop() {
+	
+	int a;
+	if (!stack1.empty())
+	{
+		while (stack1.empty())
+		{
+			a = stack1.top();
+			stack2.push(a);
+			stack1.pop();
+		}
+	}
+	a = stack2.top();
+    stack2.pop();
+	return a;
+
+}*/
+
+//输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+//使用递归方法
+/*ListNode* Solution::Merge(ListNode* pHead1, ListNode* pHead2)
+{
+	ListNode * Node = NULL;
+	if (pHead1 == NULL)
+		return pHead2;
+	if (pHead2 == NULL)
+		return pHead1;
+	if (pHead1->val <= pHead2->val)
+	{
+		Node = pHead1;
+		pHead1 = pHead1->next;
+		Node->next = Merge(pHead1, pHead2);
+	}
+	else
+	{
+		Node = pHead2;
+		pHead2 = pHead2->next;
+		Node->next = Merge(pHead1, pHead2);
+		
+	}
+	return Node;
+}*/
+
--- ConsoleApplication1/ConsoleApplication1/pointToOffer.h
+++ ConsoleApplication1/ConsoleApplication1/pointToOffer.h
@@ -1,13 +1,31 @@
 #pragma once
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {}
+};
 class Solution
 {
 public:
+
 //	int  NumberOf1(int n) {}
 
-	void replaceSpace(char *str, int length);
+//	void replaceSpace(char *str, int length);
+
+//	void push(int node);
+//	int pop();
+	ListNode* Merge(ListNode* pHead1, ListNode* pHead2);
+	
+
+
+private:
+//	stack<int> stack1;
+//	stack<int> stack2;
 
 };
